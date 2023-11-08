36b7f656939e772fd673e0ac9749a3fdbfbe5a05
Robson William <robsonwilliamro@gmail.com>
2023-11-08 11:27:12 -0300

    Mudando css dinamicamente

--- src/components/Model/Model.tsx
+++ src/components/Model/Model.tsx
@@ -13,30 +13,47 @@ type Hortalica = {
     unitsNeedToSell: number
 }
 
+type Measures = {
+    width: number,
+    height: number,
+    area: number,
+    freeSpace: number,
+    productiveUnit: number,
+    hortalica: Hortalica,
+    production: number
+}
+
 type Props = {
-    info: {
-        width: number,
-        height: number,
-        area: number,
-        freeSpace: number,
-        productiveUnit: number,
-        hortalica: Hortalica,
-        production: number
-    }
+    info: Measures
 }
 
 export default function Model({info}:Props){
+    const calcProductiveUnitPerColumn = (info:Measures) => {
+        return Math.trunc((info.width * 0.75) / info.productiveUnit);
+    }
+
+    const gridStyle = {
+        display: "grid",
+        gridTemplateColumns: `repeat(${calcProductiveUnitPerColumn(info)}, 100px)`,
+        gridTemplateRows: `repeat(${calcProductiveUnitPerColumn(info)}, 100px)`,
+        rowGap: "30px",
+        width: "100%",
+        maxHeight: "100%",
+        padding: "30px"
+    }
+
     const arrayLength = Math.trunc(info.production / (info.hortalica.unitArea * 100));
-    
     const keys = [...Array(arrayLength).keys()];
 
     return (
         <>
-            {
-                keys.map((item) => (
-                    <ProductiveUnit info={info} key={item}/>
-                ))
-            }
+            <div style={gridStyle}>
+                {
+                    keys.map((item) => (
+                        <div className="productive-unit-div"  key={item}><ProductiveUnit info={info}/></div>
+                    ))
+                }
+            </div>
         </>
     )
 }
